95021e837244d55bc6379f98ed6bd4fc3dfefb9c
Jean-François Fortin Tam <nekohayo@gmail.com>
2012-03-01 23:19:42 -0500

    title_editor_canvas: Document the do_set_property method

--- pitivi/dialogs/title_editor_canvas.py
+++ pitivi/dialogs/title_editor_canvas.py
@@ -139,6 +139,9 @@ class TitlePreview(gtk.EventBox):
             raise AttributeError
 
     def do_set_property(self, property, value):
+        """
+        For some properties, handle additional things to be done they change.
+        """
         # Use if clauses with "hasattr" to ensure that the property we're trying
         # to set really has a target object to receive it. This is necessary
         # because do_set_property may be called multiple times before the
